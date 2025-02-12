edbc3dbe7e32d7e974b9ed7b4e30a92e8585da97
rambojr10 <valenciapipe1@gmail.com>
2025-02-12 01:48:31 -0500

    Add unit tests for currency conversion

---
+++ tests/result.test.js
@@ -0,0 +1,20 @@
+import { strictEqual } from 'node:assert'
+import { test } from 'node:test'
+import { calculateResult, displayResult } from '../result.js'
+
+const data = { USD: 1, EUR: 0.90, GBP: 0.86, AUD: 1.59 }
+
+test('calculateResult should calculate the correct result when converting from default currencies', async () => {
+  const result = await calculateResult({ amount: 100, from: 'USD', to: 'EUR', data})
+  strictEqual(result, 90)
+})
+
+test('calculateResult should calculate the correct result when converting between non-default currencies', async () => {
+  const result = await calculateResult({ amount: 100, from: 'GBP', to: 'AUD', data })
+  strictEqual(result.toFixed(2), '184.88')
+})
+
+test('displayResult should display the correct result message', async () => {
+  const message = await displayResult({ amount: 100, from: 'USD', to: 'GBP', data })
+  strictEqual(message, '100 USD = 86 GBP')
+})
